980328e71df620c5646ec77d33551a1aa758a515
Mathew Kim <mkim288@uic.edu>
2018-05-07 20:10:33 +0000

    Completed Ch02_03

--- Ch02/02_03/start/app.js
+++ Ch02/02_03/start/app.js
@@ -1,11 +1,27 @@
 (function() {
   "use strict";
 
-  var ProductCustomizer = React.createElement(
-    "div",
-    { className: "customizer" },
-    "Product customizer will go here"
-  );
+  function ProductImage(props) {
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product Image"
+    });
+  }
 
-  ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      React.createElement(
+        "div",
+        { className: "product-image" },
+        React.createElement(ProductImage)
+      )
+    );
+  }
+
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
+  );
 })();
